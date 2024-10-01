ee2a2dc75f807fb719908952b22ae356034e82b0
SHXBX1 <natdanai.workspace@gmail.com>
2024-10-01 09:17:35 +0700

    up 10/1/67

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-if(isset($_SESSION['id'])){
+if (isset($_SESSION['id'])) {
     header("location:index.php");
     die();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,39 +16,53 @@ if(isset($_SESSION['id'])){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
+
 <body>
     <h1 style="text-align: center;">Webboard</h1>
-    <?php
-        if(isset($_SESSION['error'])&& S_SESSION['error']){
-            echo "<div class='alert alert-danger mt-4 me-auto ms-auto' style='width:400px'>ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง</div>";
-            unset($_SESSION['error']);
-        }    
-    ?>
-    <form action="verify.php" method="post">
     <?php include "nav.php" ?>
     <div class="container-lg">
-    <div class="card mt-4 me-auto ms-auto " style="width:18rem; background-color: #f8f4f4">
-        <div class="card-header"> เข้าสู่ระบบ </div>
-        <div class="cardbody">
-            <div class="form-gruop">
-                <lable for="login" class="form-lable">login: </lable>
-                <lable id="login" type="text" class="form-control"></lable>
+        
+        <div class="row ">
+            <div class="col-sm-8 col-md-6 col-lg-4 mx-auto">
+                <?php 
+                    if (isset($_SESSION['error']) ) {
+                        echo "<div class='alert alert-danger mt-3' role='alert'>
+                                ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง
+                                </div>";
+                        unset($_SESSION['error']);
+                    }
+                ?>
+                <div class="card mt-2">
+                    <h5 class="card-header">เข้าสู่ระบบ</h5>
+                    <div class="card-body">
+                        <form action="verify.php" method="post">
+                            <div class="form-group">
+                                <label for="login" class="form-label">Login:</label>
+                                <input id="login" type="text" name="login" class="form-control"></input>
+                            </div>
+                            <div class="form-group mt-3">
+                                <label for="pwd" class="form-label">Password:</label>
+                                <input id="pwd" type="password" class="form-control" name="pwd"></input>
+                            </div>
+                            <div class="mt-3 d-flex justify-content-center">
+                                <button type="submit" class="btn btn-success btn-sm me-2">
+                                    login
+                                </button>
+                                <button type="reset" class="btn btn-danger btn-sm">
+                                    reset
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="form-gruop">
-                <lable for="pwd" class="form-lable">Password: </lable>
-                <lable id="pwd" type="text" class="form-control"></lable>
-            </div>
-        <div>
-            <button type="submit" class="btn btn-success btn-sm">Login</button>
-            <button type="reset" class="btn btn-success btn-sm">Reset</button>
         </div>
-        </div>
-    </form>
+        
+    </div>
+
+    </div>
     <br>
-        </div>
-    <div style="text-align: center;">
-        ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a>
-    </div>
-    </div>
+    <div style="text-align: center;"> ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a> </div>
 </body>
+
 </html>
--- verify.php
+++ verify.php
@@ -20,19 +20,26 @@ if(isset($_SESSION['id'])){
 			$_SESSION['username']="admin";
 			$_SESSION['role']="a";
 			$_SESSION['id']=session_id();
-			echo "ยินดีต้อนรับคุณ ADMIN <br>";
-			echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
+			header("location:login.php");
+			die();
+			// echo "ยินดีต้อนรับคุณ ADMIN <br>";
+			// echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
 		}
 		elseif ($_POST["login"]=="member" && $_POST["pwd"]=="mem1234"){
 			$_SESSION['username']="member";
 			$_SESSION['role']="m";
 			$_SESSION['id']=session_id();
-			echo "ยินดีต้อนรับคุณ MEMBER <br>";
-			echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
+			header("location:login.php");
+			die();
+			// echo "ยินดีต้อนรับคุณ MEMBER <br>";
+			// echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
 		}
 		else{
-			echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง <br>";
-			echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
+			$_SESSION['error']='error';
+			header("location:login.php");
+			die();
+			// echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง <br>";
+			// echo "<a href="."index.php".">กลับไปยังหน้าหลัก</a>";
 		}
 	?>
 	</center>
